cc17e1b1851bb81938cca3985c0f1da0642e4e70
makin <makin.ugm@gmail.com>
2026-02-07 11:36:45 +0700

    add search-eval prompt builder

--- scripts/search.py
+++ scripts/search.py
@@ -4,15 +4,20 @@ from pyserini.search.lucene import LuceneSearcher
 
 searcher = LuceneSearcher('indexes/lucene-index-pubmed')
 
-# Ambil query dari argumen atau input interaktif
-if len(argv) > 1:
-    query = " ".join(argv[1:])
-else:
-    query = input("Masukkan query penyakit: ")
+def main(query):
+    hits = searcher.search(f"{query} AND (treatment OR pharmacotherapy OR drug OR therapy)", k=10)
 
-hits = searcher.search(f"{query} AND (treatment OR pharmacotherapy OR drug OR therapy)", k=10)
+    hasil = ""
+    for hit in hits:
+        hasil += f"{hit.docid} | {hit.score:.4f}\n"
+        doc = searcher.doc(hit.docid)
+        hasil += doc.raw() + "\n"  # JSON lengkap
+    return hasil
 
-for hit in hits:
-    print(f"{hit.docid} | {hit.score:.4f}")
-    doc = searcher.doc(hit.docid)
-    print(doc.raw())  # JSON lengkap
+if __name__ == "__main__":
+    # Ambil query dari argumen atau input interaktif
+    if len(argv) > 1:
+        query = " ".join(argv[1:])
+    else:
+        query = input("Masukkan query penyakit: ")
+    print(main(query))
